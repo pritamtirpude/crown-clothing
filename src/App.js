4c2c1f44f74d717fd927c83b01c51021f77ae4a4
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-08-04 17:47:49 +0530

    update the menuitem component withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage.component";
 
 function App() {
+  const HatsPage = () => {
+    return (
+      <div>
+        <h1>Hats Page</h1>
+      </div>
+    );
+  };
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
+      <Route path="/shop/hats" component={HatsPage} />
     </div>
   );
 }
