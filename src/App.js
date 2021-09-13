9a82cf5bbb246dcbb866a8146cec8c271334489a
Pritam Tirpude <pritam.tirpude9601@gmail.com>
2021-09-13 17:29:57 +0530

    added addItem and removeItem functionality in checkout item compoment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import { createStructuredSelector } from "reselect";
 import { Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 import { setCurrentUser } from "./components/redux/user/user.actions";
+import { selectCurrentUser } from "./components/redux/user/user.selectors";
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -44,6 +46,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -61,8 +64,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
